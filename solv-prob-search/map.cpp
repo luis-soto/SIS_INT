0281abc8cb0b925bc3e1dfd73e9ebd130b182335
luis.soto <luisfelipesotolauro@hotmail.com>
2019-03-31 23:07:15 -0300

    Implements the map class to read from .txt file and generate map matrix

--- solv-prob-search/map.cpp
+++ solv-prob-search/map.cpp
@@ -1,1 +1,57 @@
 #include "map.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// -------------------------------------------------------------------------------------------------
+Map::Map()
+{
+    ifstream mapFile("./map.txt");
+
+    if(!mapFile)
+    {
+        cerr << "Unable to open file map.txt" << endl;
+        exit(1);
+    }
+    else
+    {
+        string line;
+        char ch;
+
+        getline(mapFile,line);
+        this->Rows = stoi(line);
+        getline(mapFile,line);
+        this->Cols = stoi(line);
+
+        this->map = new char*[Rows]();
+            
+        for(int idx = 0; idx < Rows; idx++)
+        {
+            this->map[idx] = new char[Cols]();
+        }
+
+        int r = 0;
+        int c = 0;
+
+        while(mapFile.get(this->map[r][c]))
+        {
+            if('\n' != this->map[r][c])
+            {
+                c++;
+                if(this->Cols <= c)
+                {
+                    r++;
+                    c = 0;
+                    if(this->Rows <= r)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        mapFile.close();
+    }
+}
--- solv-prob-search/map.h
+++ solv-prob-search/map.h
@@ -1,5 +1,38 @@
 #ifndef MAP_H
 #define MAP_H
 
+// Class that define the map
+class Map
+{
+    private:
+        char **map;
+        int Rows;
+        int Cols;
+
+    public:
+    Map();
+
+    ~Map()
+    {
+        
+    }
+
+    void Display();
+
+    int GetRows() const
+    {
+        return this->Rows;
+    }
+
+    int GetCols() const
+    {
+        return this->Cols;
+    }
+
+    char GetItemByPos(int r, int c) const
+    {
+        return this->map[r][c];
+    }
+};
 
 #endif // MAP_H
