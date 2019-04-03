9f60e29513eb915d42e8fd45767557c6fc79d2b6
luis.soto <luisfelipesotolauro@hotmail.com>
2019-04-02 21:33:37 -0300

    Add new features to class tree and new functions to access attributes

--- solv-prob-search/tree.cpp
+++ solv-prob-search/tree.cpp
@@ -1,1 +1,100 @@
 #include "tree.h"
+#include <stdlib.h>							// abs()
+
+using namespace std;
+
+// -------------------------------------------------------------------------------------------------
+void Node::AddSuccessor(Node* newSuccessor)
+{
+	this->successors.push_back(newSuccessor);
+}
+
+// -------------------------------------------------------------------------------------------------
+Node* Node::GetSuccessor(int idx)
+{
+	Node* ret = nullptr;
+	
+	if(this->successors.size() > idx)
+	{
+		ret = this->successors[idx];
+	}
+	else
+	{
+		// Node don't have this number of successors
+	}
+	
+	return ret;
+}
+
+// -------------------------------------------------------------------------------------------------
+void Tree::FindSuccessors(Node* parent)
+{
+	if(nullptr != parent)
+	{
+		vector<int> adjNodes;
+		int parentIdx = this->graph->NodeIsMember(parent->GetState()->GetX(), parent->GetState()->GetY(),
+												  parent->GetState()->GetDir());
+		
+		// Determine adjacent nodes (interpreted as children in the tree)
+		for(int adjIdx = 0; adjIdx < graph->GetExpectedNumOfNodes(); adjIdx++)
+		{
+			if(graph->IsAdjacent(parentIdx, adjIdx))
+			{
+				adjNodes.push_back(adjIdx);
+			}
+		}
+
+		// Create nodes and add them to the successors vector of the parent node
+		for(int idx = 0; idx < adjNodes.size(); idx++)
+		{
+			Action action = DetermineAction(adjNodes[idx], parent);
+
+			Node* newNode = new Node(graph->GetStateByIdx(adjNodes[idx]), parent,
+									(parent->GetG())+graph->GetEdgeWeigthByIdx(parentIdx, adjNodes[idx]), action);
+
+			if(nullptr != newNode)
+			{
+				parent->AddSuccessor(newNode);
+			}
+		}
+
+		adjNodes.clear();
+	}
+	else
+	{
+		// Isn't a valid node
+	}
+}
+
+// -------------------------------------------------------------------------------------------------
+Action Tree::DetermineAction(int idx, Node* parent)
+{
+	Action action;
+
+	if((1 == abs(graph->GetStateByIdx(idx)->GetX()-parent->GetState()->GetX())) ||
+		(1 == abs(graph->GetStateByIdx(idx)->GetY()-parent->GetState()->GetY())))
+	{
+		action = Action::FRONT;
+	}
+	else
+	{
+		if((1 == (graph->GetStateByIdx(idx)->GetDir()-parent->GetState()->GetDir())) ||
+			((Direction::W-Direction::NW) == (graph->GetStateByIdx(idx)->GetDir()-parent->GetState()->GetDir())))
+		{
+			action = Action::TURN_RIGHT;
+		}
+		else
+		{
+			if((-1==(graph->GetStateByIdx(idx)->GetDir()-parent->GetState()->GetDir())) ||
+				((Direction::NW-Direction::W) == (graph->GetStateByIdx(idx)->GetDir()-parent->GetState()->GetDir())))
+			{
+				action = Action::TURN_LEFT;
+			}
+			else
+			{
+				action = Action::NO_ACTION;
+			}
+		}
+	}
+	return action;
+}
--- solv-prob-search/tree.h
+++ solv-prob-search/tree.h
@@ -1,6 +1,144 @@
-#ifndef TREE_H
-#define TREE_H
+#ifndef __TREE_H
+#define __TREE_H
+
+#include "graph.h"
+#include <vector>
 
 using namespace std;
 
-#endif // TREE_H
+enum Action
+{
+    TURN_LEFT,
+    TURN_RIGHT,
+    FRONT,
+    NO_ACTION
+};
+
+class Node
+{
+	private:
+		State* state;
+		bool visited;
+        Node* parent;
+		vector<Node*> successors;
+        // The necessary action to get to this node from the parent node
+        Action action;
+        // heuristic function for the node
+        int f;
+        // path cost till this node
+        int g;
+
+	public:
+        Node(State* state, Node* parent = nullptr, int g = 0, Action action = NO_ACTION, int f = 0, bool visited = false)
+        {
+            this->state = state;
+            this->visited = visited;
+            this->parent = parent;
+            this->f = f;
+            this->g = g;
+            this->action = action;
+        }        
+
+		~Node()
+		{
+			
+		}
+
+        void AddSuccessor(Node* newSuccessor);
+
+        Action GetAction()
+        {
+            return this->action;
+        }
+
+        bool GetVisited() const
+		{
+			return this->visited;
+		}
+		
+		int GetNumberOfSuccessors() const
+		{
+			return this->successors.size();
+		}
+
+        int GetF()
+        {
+            return this->f;
+        }
+
+        int GetG()
+        {
+            return this->g;
+        }
+
+        // IMPLEMENTED
+        Node* GetSuccessor(int idx);
+
+        // Can return Node* because Node (this class) is a class and already have encapsulation
+        Node* GetParent() const
+        {
+            return this->parent;
+        }
+
+        State* GetState() const
+        {
+            return this->state;            
+        }
+
+		void SetVisited(bool visited)
+		{
+			this->visited = visited;
+		}
+
+        void SetAction(Action action)
+        {
+            this->action = action;
+        }
+
+        void SetF(int f)
+        {
+            this->f = f;
+        }
+
+        void SetG(int g)
+        {
+            this->g = g;
+        }
+
+        void SetParent(Node* parent)
+        {
+            this->parent = parent;
+        }
+};
+
+class Tree
+{
+	private:
+		Node* root;
+        // tree class is friend of graph class
+        Graph* graph;
+
+	public:
+        // When a tree is created it must receive the pointer to the root (source) state
+        // and the associated graph
+        Tree(State* rootState, Graph* graph)
+        {
+            this->root = new Node(rootState);
+            this->graph = graph;
+        }
+
+		~Tree()
+		{
+			
+		}
+
+        // IMPLEMENTED
+        // Find the successors from the node specified and expands the tree with them
+        void FindSuccessors(Node* parent);
+
+        // Determine the action taken to get from parent to child
+        // idx is the index of the child in the graph
+        Action DetermineAction(int idx, Node* parent);
+};
+
+#endif // __TREE_H
