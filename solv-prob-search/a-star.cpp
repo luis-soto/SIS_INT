f6e0833e7695d6a97a14363cc216f33a1cd1c78b
luis.soto <luisfelipesotolauro@hotmail.com>
2019-04-14 21:03:31 -0300

    Corrects segmentation fault and finishes implementation

--- solv-prob-search/a-star.cpp
+++ solv-prob-search/a-star.cpp
@@ -1,1 +1,149 @@
+#include <cmath>
+#include <iostream>
 #include "a-star.h"
+
+// -------------------------------------------------------------------------------------------------
+AStar::AStar(ProblemFormulator* problemFormulation)
+{
+    this->_problemFormulation = problemFormulation;
+    this->_searchTree = new Tree(this->_problemFormulation->getInitialState(),
+                                this->_problemFormulation->getGraphModel());
+    this->addNodeToOpenList(this->_searchTree->getRootNode());
+    this->_actualNode = nullptr;
+    this->_visitedNodes = 0;
+}
+
+// -------------------------------------------------------------------------------------------------
+vector<Action> AStar::aStarSearch()
+{
+    bool pathFound = false;
+
+    while(!this->_openList.empty() && !pathFound)
+    {
+        this->_visitedNodes++;
+        int bestIdx = this->findBestNodeInOpenList();
+        this->_actualNode = this->_openList[bestIdx];
+
+        cout << endl;
+        displayOpenList();
+        cout << "bestIdx = " << bestIdx << endl;
+        cout << "visiting node x = " << _actualNode->getState()->getX() << endl;
+        cout << "visiting node y = " << _actualNode->getState()->getY() << endl;
+        cout << "visiting node dir = " << _actualNode->getState()->getDir() << endl;
+        cout << "visiting node f = " << _actualNode->getF() << endl;
+        cout << "visited of visiting node = " << this->_actualNode->getVisited() << endl;
+        cout << endl;
+
+        this->_actualNode->setVisited(true);
+
+        this->_openList.erase(this->_openList.begin()+bestIdx);
+        
+        if(this->isGoal())
+        {
+            pathFound = true;
+        }
+        else
+        {
+            this->_searchTree->findSuccessors(this->_actualNode);
+            for(int idx = 0; this->_actualNode->getNumberOfSuccessors() > idx; idx++)
+            {
+                Node* successor = this->_actualNode->getSuccessor(idx);
+                if(nullptr != successor)
+                {
+                    this->addNodeToOpenList(successor);
+                }
+            }
+        }
+    }
+
+    if(this->_openList.empty())
+    {
+        cout << "Path not found." << endl;
+    }
+
+    if(pathFound)
+    {
+        cout << "Path found." << endl;
+        this->_actionQueue = this->_searchTree->generateActionQueue(this->_actualNode);
+    }
+    return this->_actionQueue;
+}
+
+// -------------------------------------------------------------------------------------------------
+void AStar::addNodeToOpenList(Node* node)
+{
+    if(nullptr != node)
+    {
+        this->calculateF(node);
+        this->_openList.push_back(node);
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+void AStar::calculateF(Node* node)
+{
+    if(nullptr != node)
+    {
+        double h = sqrt(pow((this->_problemFormulation->getGoalState()->getX()-node->getState()->getX()), 2) +
+                        pow((this->_problemFormulation->getGoalState()->getY()-node->getState()->getY()), 2));
+        node->setF(node->getG()+h);
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+int AStar::findBestNodeInOpenList() const
+{
+    int bestIdx;
+
+    if(!this->_openList.empty())
+    {
+        bestIdx = 0;
+        for(int idx = 0; idx < this->_openList.size(); idx++)
+        {
+            if(this->_openList[idx]->getF() < this->_openList[bestIdx]->getF())
+            {
+                bestIdx = idx;
+            }
+        }
+    }
+    else
+    {
+        bestIdx = -1;
+    }
+
+    return bestIdx;
+}
+
+// -------------------------------------------------------------------------------------------------
+bool AStar::isGoal() const
+{
+    bool isGoal = false;
+    if(nullptr != this->_actualNode)
+    {
+        if((this->_actualNode->getState()->getX() == this->_problemFormulation->getGoalState()->getX()) &&
+            (this->_actualNode->getState()->getY() == this->_problemFormulation->getGoalState()->getY()))
+        {
+            if(Direction::DEFAULT != this->_problemFormulation->getGoalState()->getDir())
+            {
+                if(this->_actualNode->getState()->getDir() == this->_problemFormulation->getGoalState()->getDir())
+                {
+                    isGoal = true;
+                }
+            }
+            else
+            {
+                isGoal = true;
+            }
+        }
+    }
+    return isGoal;
+}
+
+void AStar::displayOpenList() const
+{
+    for(int idx = 0; this->_openList.size() > idx; idx++)
+    {
+        cout << "N" << idx+1 << "=" << this->_openList[idx]->getF() << "  ";
+    }
+    cout << endl;
+}
--- solv-prob-search/a-star.h
+++ solv-prob-search/a-star.h
@@ -1,5 +1,46 @@
-#ifndef A_START_H
-#define A_START_H
+#ifndef __A_STAR_H
+#define __A_STAR_H
 
+#include "tree.h"
+#include "problem-formulator.h"
+#include <vector>
 
-#endif // A_START_H
+using namespace std;
+
+class AStar
+{
+    private:
+        ProblemFormulator* _problemFormulation;
+        Tree* _searchTree;
+        vector<Node*> _openList;
+        int _visitedNodes;
+        Node* _actualNode;
+        vector<Action> _actionQueue;
+
+    public:
+        AStar(ProblemFormulator* problemFormulation);
+
+        ~AStar()
+        {
+            delete this->_searchTree;
+        }
+
+        vector<Action> aStarSearch();
+
+        void calculateF(Node* node);
+
+        bool isGoal() const;
+        
+        int getVisitedNodes()
+        {
+            return this->_visitedNodes;
+        }
+
+        int findBestNodeInOpenList() const;
+
+        void addNodeToOpenList(Node* node);
+
+        void displayOpenList() const;
+};
+
+#endif // __A_STAR_H
