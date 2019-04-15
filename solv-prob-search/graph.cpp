a7e174e0764ce43d0694f85d3ac580dd87885abd
luis.soto <luisfelipesotolauro@hotmail.com>
2019-04-14 21:06:25 -0300

    Changes names of methods and attributes of graph class and state class

--- solv-prob-search/graph.cpp
+++ solv-prob-search/graph.cpp
@@ -1,41 +1,40 @@
 #include "graph.h"
-#include "tree.h"
 
 using namespace std;
 
 // -------------------------------------------------------------------------------------------------
-void Graph::AddAdjacentNode(int idxOrigin, int idxEnd, int w)
+void Graph::addAdjacentNode(int idxOrigin, int idxEnd, int w)
 {
 	if((-1 != idxOrigin) && (-1 != idxEnd))
 	{
-		this->edges[idxOrigin][idxEnd] = w;
+		this->_edges[idxOrigin][idxEnd] = w;
 	}
 }
 
 // -------------------------------------------------------------------------------------------------
-void Graph::AddAdjacentNode(int originX, int originY, Direction originDir,
+void Graph::addAdjacentNode(int originX, int originY, Direction originDir,
                         int endX, int endY, Direction endDir, int w)
 {
-	int idxOrigin = this->NodeIsMember(originX, originY, originDir);
-	int idxEnd = this->NodeIsMember(endX, endY, endDir);
+	int idxOrigin = this->nodeIsMember(originX, originY, originDir);
+	int idxEnd = this->nodeIsMember(endX, endY, endDir);
 
 	if((-1 != idxOrigin) && (-1 != idxEnd))
 	{
-		this->edges[idxOrigin][idxEnd] = w;
+		this->_edges[idxOrigin][idxEnd] = w;
 	}
 }
 
 // -------------------------------------------------------------------------------------------------
-bool Graph::IsAdjacent(int originX, int originY, Direction originDir,
+bool Graph::isAdjacent(int originX, int originY, Direction originDir,
                         int endX, int endY, Direction endDir) const
 {
 	bool isAdjacent = false;
-	int idxOrigin = this->NodeIsMember(originX, originY, originDir);
-	int idxEnd = this->NodeIsMember(endX, endY, endDir);
+	int idxOrigin = this->nodeIsMember(originX, originY, originDir);
+	int idxEnd = this->nodeIsMember(endX, endY, endDir);
 
 	if((-1 != idxOrigin) && (-1 != idxEnd))
 	{
-		if(0 != this->edges[idxOrigin][idxEnd])
+		if(0 != this->_edges[idxOrigin][idxEnd])
 		{
 			isAdjacent = true;
 		}
@@ -44,25 +43,25 @@ bool Graph::IsAdjacent(int originX, int originY, Direction originDir,
 }
 
 // -------------------------------------------------------------------------------------------------
-int Graph::GetEdgeWeigthByIdx(int idxOrigin, int idxEnd) const
+int Graph::getEdgeWeigthByIdx(int idxOrigin, int idxEnd) const
 {
 	int weigth = 0;
 
-	if(IsValidEdge(idxOrigin, idxEnd))
+	if(isValidEdge(idxOrigin, idxEnd))
 	{
-		weigth = this->edges[idxOrigin][idxEnd];
+		weigth = this->_edges[idxOrigin][idxEnd];
 	}
 
 	return weigth;
 }
 
 // -------------------------------------------------------------------------------------------------
-bool Graph::IsValidEdge(int idxOrigin, int idxEnd) const
+bool Graph::isValidEdge(int idxOrigin, int idxEnd) const
 {
 	bool isValid = false;
 
-	if(((0 <= idxOrigin) && (this->GetExpectedNumOfNodes() > idxOrigin)) &&
-		((0 <= idxEnd) && (this->GetExpectedNumOfNodes() > idxEnd)))
+	if(((0 <= idxOrigin) && (this->getExpectedNumOfNodes() > idxOrigin)) &&
+		((0 <= idxEnd) && (this->getExpectedNumOfNodes() > idxEnd)))
 	{
 		isValid = true;
 	}
@@ -71,11 +70,11 @@ bool Graph::IsValidEdge(int idxOrigin, int idxEnd) const
 }
 
 // -------------------------------------------------------------------------------------------------
-bool Graph::IsAdjacent(int idxOrigin, int idxEnd) const
+bool Graph::isAdjacent(int idxOrigin, int idxEnd) const
 {
 	bool isAdjacent = false;
 
-	if(0 != this->edges[idxOrigin][idxEnd])
+	if(0 != this->_edges[idxOrigin][idxEnd])
 	{
 		isAdjacent = true;
 	}
@@ -83,27 +82,27 @@ bool Graph::IsAdjacent(int idxOrigin, int idxEnd) const
 }
 
 // -------------------------------------------------------------------------------------------------
-void Graph::DeleteNode(int x, int y, Direction dir)
+void Graph::deleteNode(int x, int y, Direction dir)
 {
-	int idx = this->NodeIsMember(x, y, dir);
+	int idx = this->nodeIsMember(x, y, dir);
 
 	if(-1 != idx)
 	{
-		this->graph.erase(this->graph.begin()+idx);
+		this->_graph.erase(this->_graph.begin()+idx);
 	}	
 }
 
 // -------------------------------------------------------------------------------------------------
-int Graph::NodeIsMember(int x, int y, Direction dir) const
+int Graph::nodeIsMember(int x, int y, Direction dir) const
 {
 	int idx = -1;
-	for(int it = 0; it < this->graph.size();it++)
+	for(int it = 0; it < this->_graph.size();it++)
 	{
-		if(this->graph[it]->GetX() == x)
+		if(this->_graph[it]->getX() == x)
 		{
-			if(this->graph[it]->GetY() == y)
+			if(this->_graph[it]->getY() == y)
 			{
-				if(this->graph[it]->GetDir() == dir)
+				if(this->_graph[it]->getDir() == dir)
 				{
 					idx = it;
 				}
@@ -114,36 +113,36 @@ int Graph::NodeIsMember(int x, int y, Direction dir) const
 }
 
 // -------------------------------------------------------------------------------------------------
-void Graph::JoinEdge(int originX, int originY, Direction originDir,
+void Graph::joinEdge(int originX, int originY, Direction originDir,
                         int endX, int endY, Direction endDir, int w)
 {	
-	int idxOrigin = this->NodeIsMember(originX, originY, originDir);
-	int idxEnd = this->NodeIsMember(endX, endY, endDir);
+	int idxOrigin = this->nodeIsMember(originX, originY, originDir);
+	int idxEnd = this->nodeIsMember(endX, endY, endDir);
 	
 	if(-1 == idxOrigin)
 	{
-		this->JoinNode(originX, originY, originDir);
-		idxOrigin = this->graph.size()-1;
+		this->joinNode(originX, originY, originDir);
+		idxOrigin = this->_graph.size()-1;
 	}
 
 	if(-1 == idxEnd)
 	{
-		this->JoinNode(endX, endY, endDir);
-		idxEnd = this->graph.size()-1;
+		this->joinNode(endX, endY, endDir);
+		idxEnd = this->_graph.size()-1;
 	}	
 	
-	if(!this->IsAdjacent(idxOrigin, idxEnd))
+	if(!this->isAdjacent(idxOrigin, idxEnd))
 	{
-		this->AddAdjacentNode(idxOrigin, idxEnd, w);
+		this->addAdjacentNode(idxOrigin, idxEnd, w);
 	}
 }
 
 // -------------------------------------------------------------------------------------------------
-bool Graph::IsValidState(int x, int y) const
+bool Graph::isValidState(int x, int y) const
 {
 	bool isValid = false;
 
-	if((0 <= x) && (this->numRows > x) && (0 <= y) && (this->numCols > y))
+	if((0 <= x) && (this->_numRows > x) && (0 <= y) && (this->_numCols > y))
 	{
 		isValid = true;
 	}
@@ -152,11 +151,11 @@ bool Graph::IsValidState(int x, int y) const
 }
 
 // -------------------------------------------------------------------------------------------------
-void Graph::JoinNode(int x, int y, Direction dir)
+void Graph::joinNode(int x, int y, Direction dir)
 {
-	if(this->IsValidState(x, y))
+	if(this->isValidState(x, y))
 	{
-		const int idx = this->NodeIsMember(x, y, dir);
+		const int idx = this->nodeIsMember(x, y, dir);
 
 		// Node does not exist already
 		if(-1 == idx)
@@ -164,21 +163,21 @@ void Graph::JoinNode(int x, int y, Direction dir)
 			State* newNode = new State(x, y, dir);
 			if(nullptr != newNode)
 			{
-				this->graph.push_back(newNode);
+				this->_graph.push_back(newNode);
 			}
 		}
 	}
 }
 
 // -------------------------------------------------------------------------------------------------
-void Graph::RemoveEdge(int originX, int originY, Direction originDir,
+void Graph::removeEdge(int originX, int originY, Direction originDir,
                         int endX, int endY, Direction endDir)
 {
-	int idxOrigin = this->NodeIsMember(originX, originY, originDir);
-	int idxEnd = this->NodeIsMember(endX, endY, endDir);
+	int idxOrigin = this->nodeIsMember(originX, originY, originDir);
+	int idxEnd = this->nodeIsMember(endX, endY, endDir);
 
 	if((-1 != idxEnd) && (-1 != idxOrigin))
 	{
-		this->edges[idxOrigin][idxEnd] = 0;
+		this->_edges[idxOrigin][idxEnd] = 0;
 	}
 }
--- solv-prob-search/graph.h
+++ solv-prob-search/graph.h
@@ -30,16 +30,16 @@ enum Direction
 class State
 {
     private:
-		int x;
-        int y;
-        Direction dir;
+		int _x;
+        int _y;
+        Direction _dir;
     
     public:
         State(int x = 0, int y = 0, Direction dir = Direction::DEFAULT)
         {
-            this->x = x;
-            this->y = y;
-            this->dir = dir;
+            this->_x = x;
+            this->_y = y;
+            this->_dir = dir;
         }
 
         ~State()
@@ -47,34 +47,34 @@ class State
 
         }
 
-        Direction GetDir()
+        Direction getDir()
         {
-            return this->dir;
+            return this->_dir;
         }
 
-        int GetX()
+        int getX()
         {
-            return this->x;
+            return this->_x;
         }
 
-        int GetY()
+        int getY()
         {
-            return this->y;
+            return this->_y;
         }
 
-        void SetDir(Direction dir)
+        void setDir(Direction dir)
         {
-            this->dir = dir;
+            this->_dir = dir;
         }
 
-        void SetX(int x)
+        void setX(int x)
         {
-            this->x = x;
+            this->_x = x;
         }
 
-        void SetY(int y)
+        void setY(int y)
         {
-            this->y = y;
+            this->_y = y;
         }
             
 };
@@ -83,84 +83,90 @@ class State
 class Graph
 {
 	private:
-        int** edges;
-		vector<State*> graph;
-        int numCols, numRows, numDir;
+        int** _edges;
+		vector<State*> _graph;
+        int _numCols, _numRows, _numDir;
 
 	public:
         Graph(int numCols, int numRows, int numDir = 8)
         {
             // Create Adjancency-matrix and initializes to 0
-            edges = new int*[numCols*numDir*numRows]();
+            this->_edges = new int*[numCols*numDir*numRows]();
             
             for(int it = 0; it < numCols*numDir*numRows; it++)
             {
-                edges[it] = new int[numCols*numDir*numRows]();
+                _edges[it] = new int[numCols*numDir*numRows]();
             }
             
-            this->numCols = numCols;
-            this->numDir = numDir;
-            this->numRows = numRows;
+            this->_numCols = numCols;
+            this->_numDir = numDir;
+            this->_numRows = numRows;
         }
 
         ~Graph()
         {
-            for(int it = 0; it < this->numCols*this->numDir*this->numRows; it++)
+            for(int it = 0; it < getExpectedNumOfNodes(); it++)
             {
-                delete [] this->edges[it];
+                delete [] this->_edges[it];
             }
-            delete [] this->edges;
-            this->graph.clear();
+            delete [] this->_edges;
+            
+            this->_graph.clear();
         }
 
         // Can return State* because State is a class and already has encapsulation
-        State* GetStateByIdx(int idx) const
+        State* getStateByIdx(int idx) const
         {
             State* retState = nullptr;
 
-            if((0 <= idx) && (this->graph.size() > idx))
+            if((0 <= idx) && (this->_graph.size() > idx))
             {
-                retState = this->graph[idx];
+                retState = this->_graph[idx];
             }
 
             return retState;
         }
 
-        int GetEdgeWeigthByIdx(int idxOrigin, int idxEnd) const;
+        int getEdgeWeigthByIdx(int idxOrigin, int idxEnd) const;
 
         // Returns the maximum number of nodes suported by the graph
-        int GetExpectedNumOfNodes() const
+        int getExpectedNumOfNodes() const
         {
-            return this->numRows*this->numCols*this->numDir;
+            return this->_numRows*this->_numCols*this->_numDir;
         }
 
-        bool IsValidEdge(int idxOrigin, int idxEnd) const;
+        int getNumStates()
+        {
+            return this->_graph.size();
+        }
 
-        bool IsAdjacent(int idxOrigin, int idxEnd) const;
+        bool isValidEdge(int idxOrigin, int idxEnd) const;
 
-        void AddAdjacentNode(int idxOrigin, int idxEnd, int w = 1);
+        bool isAdjacent(int idxOrigin, int idxEnd) const;
 
-        void AddAdjacentNode(int originX, int originY, Direction originDir,
+        void addAdjacentNode(int idxOrigin, int idxEnd, int w = 1);
+
+        void addAdjacentNode(int originX, int originY, Direction originDir,
                         int endX, int endY, Direction endDir, int w = 1);
 
         // Check whether or not x and y are in the range of possible states
-        bool IsValidState(int x, int y) const;
+        bool isValidState(int x, int y) const;
 
-        bool IsAdjacent(int originX, int originY, Direction originDir,
+        bool isAdjacent(int originX, int originY, Direction originDir,
                         int endX, int endY, Direction endDir) const;
 
-        void DeleteNode(int x, int y, Direction dir);
+        void deleteNode(int x, int y, Direction dir);
 
-        void JoinEdge(int originX, int originY, Direction originDir,
+        void joinEdge(int originX, int originY, Direction originDir,
                         int endX, int endY, Direction endDir, int w = 1);
         
-        void JoinNode(int x, int y, Direction dir);
+        void joinNode(int x, int y, Direction dir);
         
         // Verifies whether or not the node specified by <x,y,dir> belongs to the graph
         // Returns the index of the node if belongs to the graph and -1 id doesn't
-        int NodeIsMember(int x, int y, Direction dir) const;
+        int nodeIsMember(int x, int y, Direction dir) const;
 
-        void RemoveEdge(int originX, int originY, Direction originDir,
+        void removeEdge(int originX, int originY, Direction originDir,
                             int endX, int endY, Direction endDir);
 };
 
